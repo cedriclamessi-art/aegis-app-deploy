0fcbeaed3eedc66d20cc6849574e6535d0918fa6
jonathan Lamessi <jl@MacBook-Air-de-jonathan.local>
2026-03-20 23:51:33 +0100

    Ignore TypeScript and ESLint errors during build

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   experimental: {
     serverComponentsExternalPackages: ['prisma', '@prisma/client'],
   },
